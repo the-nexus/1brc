69df74c79cb164fbc83b9c07dd74f51ebde4ae8d
Kevin Pulgar-Turcotte <kevin.pulgarturcotte@gmail.com>
2024-06-22 11:59:35 -0400

    First version of Solution A, running in about 49.5s on 1B lines without verbose

--- source/onebrc.cpp
+++ source/onebrc.cpp
@@ -4,11 +4,8 @@
 #include <filesystem>
 #include <iostream>
 
+#include "chrono_utils.h"
 #include "solution_a.h"
-
-#ifndef NO_CHRONO
-#include <chrono>
-#endif
 
 int main(int argc, char* argv[])
 {
@@ -18,36 +15,20 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    const std::filesystem::path filePath {  };
+    //auto fileProcessor = [&filePath]() { solution_a::process_file(filePath); };
+
 #ifdef NO_CHRONO
 
-    solution_a::display(argv[1]);
+    solution_a::process_file(argv[1]);
 
 #else // NO_CHRONO
 
-    const auto start = std::chrono::high_resolution_clock::now();
-    solution_a::display(argv[1]);
-    const auto end = std::chrono::high_resolution_clock::now();
-
-    const std::chrono::duration<double> deltaTime {end - start};
+    const auto dt = chrono_utils::measure_execution(&solution_a::process_file, argv[1]);
     std::cout << "Completed in ";
+    chrono_utils::display(dt);
+    std::cout << std::endl;
 
-    const int64_t deltaTimeM = std::chrono::duration_cast<std::chrono::minutes>(deltaTime).count();
-    const int64_t deltaTimeS = std::chrono::duration_cast<std::chrono::seconds>(deltaTime).count();
-    const int64_t deltaTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(deltaTime).count();
-
-    const int64_t partS = deltaTimeS - deltaTimeM * 60;
-    const int64_t partMs = deltaTimeUs - deltaTimeS * 1'000'000;
-
-    const double seconds = static_cast<double>(partS) + static_cast<double>(partMs) / 1'000'000.0;
-
-    if (deltaTimeM > 0)
-    {
-        std::cout << deltaTimeM << "m";
-    }
-    
-    std::cout << seconds << "s" << std::endl;
-    
 #endif // NO_CHRONO
-
     return 0;
 }
--- source/solution_a.h
+++ source/solution_a.h
@@ -1,29 +1,104 @@
 
 #pragma once
 
+#include <algorithm>
+#include <charconv>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <limits>
+#include <unordered_map>
 #include <string>
+#include <string_view>
+#include <utility>
 
 namespace solution_a
 {
-    void display(const std::filesystem::path& filePath)
+    constexpr unsigned short maxStationCount = 10'000;
+
+    struct station_data_t
     {
-        std::ifstream file(filePath);
+        station_data_t() = delete;
+        station_data_t(const float value)
+            : minValue(value)
+            , maxValue(value)
+            , sumValue(value)
+            , sumCount(1)
+            {}
+
+        float minValue = std::numeric_limits<float>::max();
+        float maxValue = std::numeric_limits<float>::min();
+        float sumValue = 0.f;
+        unsigned short sumCount = 0;
+    };
+
+    void process_file(const std::filesystem::path& filePath)
+    {
+        std::ifstream file {filePath};
         if (!file)
         {
             std::cerr << "Could not read file " << std::quoted(filePath.c_str()) << std::endl;
             return;
         }
 
+        std::unordered_map<size_t, std::string> labelMap;
+        labelMap.reserve(maxStationCount);
+
+        std::unordered_map<size_t, station_data_t> dataMap;
+        dataMap.reserve(maxStationCount);
+
         std::string line;
         while (std::getline(file, line))
         {
-            std::cout << line << "\n";
+            const auto separatorIt = std::find_if(std::cbegin(line), std::cend(line), [](const char c) { return c == ';'; });
+
+            std::string_view labelView = line;
+            labelView.remove_suffix(std::distance(separatorIt, std::cend(line)));
+
+            std::string_view valueView = line;
+            valueView.remove_prefix(std::distance(std::cbegin(line), separatorIt) + 1);
+
+            float value;
+            std::from_chars(valueView.data(), valueView.data() + valueView.size(), value);
+
+            const size_t labelHash = std::hash<std::string_view>{}(labelView);
+            const auto [it, emplaced] = dataMap.try_emplace(labelHash, value);
+
+            if (!emplaced)
+            {
+                station_data_t& data = it->second;
+                data.minValue = std::min(data.minValue, value);
+                data.maxValue = std::max(data.maxValue, value);
+                data.sumValue += value;
+                ++data.sumCount;
+            }
+            else
+            {
+                std::string label {labelView};
+                labelMap.emplace(labelHash, std::move(label));
+            }
         }
 
-        std::cout << std::endl;
         file.close();
+
+        bool isFirstEntry = true;
+        std::cout << "{";
+        for (const auto& [labelHash, data] : dataMap)
+        {
+            std::cout << (isFirstEntry ? "\n" : ",\n");
+            isFirstEntry = false;
+
+            const float meanValue = data.sumValue / static_cast<float>(data.sumCount);
+            const std::string& label = labelMap[labelHash];
+            
+            std::cout << std::quoted(label)
+                << ": { "
+                << "\"min\": " << data.minValue
+                << ", \"max\": " << data.maxValue
+                << ", \"mean\": " << meanValue
+                << " }";
+        }
+        std::cout << "\n}" << std::endl;
     }
 };
